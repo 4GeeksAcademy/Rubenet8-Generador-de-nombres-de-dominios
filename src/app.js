9a56af5add78d41bd93f64a1dcf43cbf257ccab4
Rubeen.Frr8 <rubenferrermascarell@gmail.com>
2025-01-06 15:58:34 +0000

    Ejercicio por solucionar

--- src/app.js
+++ src/app.js
@@ -5,26 +5,54 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   let pronoun = ("the", "our");
   let adj = ("great", "big");
   let noun = ("jogger", "racoon");
 
-  let listForLoop = "";
+  let listForLoop = ''
   for (let index = 0; index < pronoun.length; index++) {
     for (let indexAdj = 0; indexAdj < adj.length; indexAdj++) {
       for (let indexNoun = 0; indexNoun < noun.length; indexNoun++) {
         listForLoop += `
-        < li class="list-group-item" > ${pronoun[index]}${adj[indexAdj]}${noun[indexNoun]}</li >`;
+        <li class="list-group-item"> ${pronoun[index]}${adj[indexAdj]}${noun[indexNoun]}</li>`
       }
     }
   }
 
-  let listForIn = "";
+  let listForIn = ''
   for (const keyPronoun in pronoun) {
-    listForIn += `<li class= "list-group-item">${pronoun[keyPronoun]}</li>`;
+    for (const keyAdj in adj) {
+      for (const keyNoun in noun) {
+
+        listForIn += `<li class= "list-group-item">${pronoun[keyPronoun]}${adj[keyAdj]}${noun[keyNoun]}.com</li>`
+      }
+    }
   }
+
+  let listForOf = ''
+  for (const elementPronoun of pronoun) {
+    for (const elementAdj of adj) {
+      for (const elementNoun of noun) {
+        listForOf += `<li class="list-group-item">${elementPronoun}${elementAdj}${elementNoun}.es</li>`
+      }
+    }
+  }
+
+  let listMap = ''
+  pronoun.map((itemPronoun) => {
+    let domain = '.net'
+    adj.map((itemAdj) => {
+      noun.map((itemNoun) => {
+        listMap += `
+<li class="list-group-item">${itemPronoun}${itemAdj}}${itemNoun}${domain}</li>`
+
+      })
+    })
+  })
 
   document.querySelector("#for-loop").innerHTML = listForLoop;
   document.querySelector("#for-in").innerHTML = listForIn;
+  document.querySelector("#for-of").innerHTML = listForOf;
+  document.querySelector("#map").innerHTML = listMap;
 };
