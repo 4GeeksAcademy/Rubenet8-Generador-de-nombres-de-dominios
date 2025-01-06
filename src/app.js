4aa9fd7ba3f6cdd843c3d4d338fc3302564f7eb1
Rubeen.Frr8 <rubenferrermascarell@gmail.com>
2025-01-06 14:33:08 +0000

    bug:prettier

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ("the", "our");
+  let adj = ("great", "big");
+  let noun = ("jogger", "racoon");
+
+  let listForLoop = "";
+  for (let index = 0; index < pronoun.length; index++) {
+    for (let indexAdj = 0; indexAdj < adj.length; indexAdj++) {
+      for (let indexNoun = 0; indexNoun < noun.length; indexNoun++) {
+        listForLoop += `
+        < li class="list-group-item" > ${pronoun[index]}${adj[indexAdj]}${noun[indexNoun]}</li >`;
+      }
+    }
+  }
+
+  let listForIn = "";
+  for (const keyPronoun in pronoun) {
+    listForIn += `<li class= "list-group-item">${pronoun[keyPronoun]}</li>`;
+  }
+
+  document.querySelector("#for-loop").innerHTML = listForLoop;
+  document.querySelector("#for-in").innerHTML = listForIn;
 };
